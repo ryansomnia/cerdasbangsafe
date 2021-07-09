781ce19990b967799da286df4c824772fd303bdd
70001511952020 <heriyanto.sitorus@brilife.co.id>
2021-07-09 11:37:15 +0700

    xz

--- src/Component/page/MenuUtama/menuUtama.js
+++ src/Component/page/MenuUtama/menuUtama.js
@@ -1,15 +1,22 @@
 import { Component } from 'react'
-
+import { Image } from 'react-bootstrap';
+import Footer from "../../Footer/Footer";
+import Header from "../../Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class menuUtama extends Component {
     render() {
         return (
-            <div className="body">
-            <p>Menu utama</p>
-            {/* foto Profil
+            <div>
+           <Header/>
+           <Image src="holder.js/171x180" roundedCircle />
+            foto Profil
+
             biodata
             status pendaftaran
             status SPP 
-            status iuran kelas */}
+            status iuran kelas
+          <Footer/>
+          
         </div>
         )
     }
--- src/Molekul/Banner/BannerRegist.js
+++ src/Molekul/Banner/BannerRegist.js
@@ -7,58 +7,13 @@ export class Banner extends Component {
     render() {
       return (
         <div className="bentuk">
-        <Container md-4 >
-      <h1 id="tanda">"</h1> 
-       <h1 id="judul">SDS Cerdas Bangsa … Siap Mendidik anak Anda!</h1>
+        <Container md-4 className="bg-primary" >
+    
+       <h1 id="judul">Pendaftaran Siswa Baru</h1>
         <h1 id="subjudul">Tahun ajaran 2030/2031, Fasilits lengkap, Pengajar yang terakreditasi.<br></br>
            Alamat Sekolah, Tlpn: 000 000 000, Email: kitapenuliscilik@gmail.com,
             Web: Penuliscilik.com.</h1>
        <Button>Daftar</Button>
-
-        {/* <Row >
-        <Col  sm="4" >
-        <Card style={{ width: '18rem' }}>
-         <Card.Img variant="top" src="holder.js/100px180" />
-         <Card.Body>
-          <Card.Title>Judul Situ</Card.Title>
-          <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-</Col>
-
-<Col sm="4">
-        <Card style={{ width: '18rem' }}>
-         <Card.Img variant="top" src="holder.js/100px180" />
-         <Card.Body>
-          <Card.Title>Judul Apa</Card.Title>
-          <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-</Col>
-
-<Col sm="4">
-        <Card style={{ width: '18rem' }}>
-         <Card.Img variant="top" src="holder.js/100px180" />
-         <Card.Body>
-          <Card.Title>Judul Ini</Card.Title>
-          <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-</Col>
-</Row> */}
-
       </Container>
       </div>
       );
