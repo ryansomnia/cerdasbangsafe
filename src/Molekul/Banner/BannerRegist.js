9b36d7875d764cb0d78eb3ad7e5f68ba7c38f9c1
wanitalugu <mutiaraoktaviasembiring@gmail.com>
2021-07-13 12:16:22 +0700

    Banner Regist

--- src/Molekul/Banner/BannerRegist.js
+++ src/Molekul/Banner/BannerRegist.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container,Button} from 'react-bootstrap';
+import { Container,Button, Card, Accordion, Form} from 'react-bootstrap';
 import "./BannerRegist.css";
 
 
@@ -14,7 +14,64 @@ export class Banner extends Component {
            Alamat Sekolah, Tlpn: 000 000 000, Email: kitapenuliscilik@gmail.com,
             Web: Penuliscilik.com.</h1>
         
-       <Button  variant='btn btn-outline-dark'>Daftar</Button>
+  <Accordion defaultActiveKey="0">
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="dark" eventKey="0">
+        Daftar!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="0">
+      <Card.Body>
+      <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Nama Siswa</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Jenis Kelamin</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Agama</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Tanggal Lahir</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Tempat Lahir</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Alamat</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Nomor Handphone</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+      </Card.Body>
+    </Accordion.Collapse>
+  </Card>
+    </Accordion>
+
+      {/* <Button  variant='btn btn-outline-dark'>Daftar</Button> */}
       </Container>
       </div>
       );
