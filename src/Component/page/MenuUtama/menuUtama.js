58f2abd9082b620411bca2e69eeb6bd129f351b8
andreaspsrb <pasaribuandreas622@gmail.com>
2021-07-21 18:32:17 +0700

    zz

--- src/Component/page/MenuUtama/menuUtama.js
+++ src/Component/page/MenuUtama/menuUtama.js
@@ -6,11 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class menuUtama extends Component {
     render() {
         return (
-            <div>
+            <div>a
            <Header/>
            <Image src="holder.js/171x180" roundedCircle />
             foto Profil
-
+ 
             biodata
             status pendaftaran
             status SPP 
